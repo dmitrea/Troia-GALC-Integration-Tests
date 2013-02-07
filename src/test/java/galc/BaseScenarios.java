96141137eae0d088c2b6a68d61429b090bb832f5
Dana Mitrea <dana.mitrea@gmail.com>
2013-02-07 19:41:38 +0200

    Updated test_GenerateSyntheticData (according to the latest Troia-Server code updates)

--- src/test/java/galc/BaseScenarios.java
+++ src/test/java/galc/BaseScenarios.java
@@ -1,8 +1,16 @@
 package test.java.galc;
+import java.util.Map;
+
 import org.junit.Test;
 
+import com.datascience.core.base.ContValue;
+import com.datascience.core.base.LObject;
+import com.datascience.core.base.Worker;
 import com.datascience.galc.dataGenerator.SyntheticData;
-import com.datascience.galc.*;
+import com.datascience.galc.dataGenerator.SyntheticDataGenerator;
+import com.datascience.galc.ContinuousIpeirotis;
+import com.datascience.galc.DatumContResults;
+import com.datascience.galc.WorkerContResults;
 
 public class BaseScenarios {
 	
@@ -11,13 +19,15 @@ public class BaseScenarios {
 		String fileSeparator = System.getProperty("file.separator");
 		String syntheticDataFile = System.getProperty("user.dir").concat(fileSeparator).concat("datasets").concat(fileSeparator).concat("synthetic-options.txt");
 		
-		SyntheticData data = new SyntheticData(false, syntheticDataFile);
-		data.initDataParameters();
-		data.initWorkerParameters();
-		data.build();
+		SyntheticData data = new SyntheticDataGenerator(syntheticDataFile).generate();
 		
 		ContinuousIpeirotis contIpeirotis = new ContinuousIpeirotis();
 		contIpeirotis.setData(data);		
+		
+		Map<LObject<ContValue>, DatumContResults> objectsResults = contIpeirotis.getObjectsResults();
+		Map<Worker<ContValue>, WorkerContResults> workersResults = contIpeirotis.getWorkersResults();
+		System.out.println(objectsResults);
+		System.out.println(workersResults);
 	}
 
 }
